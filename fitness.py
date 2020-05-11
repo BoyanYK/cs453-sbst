98df048627c44f33c621ef0a702d601e92784e79
BoyanYK <28277932+BoyanYK@users.noreply.github.com>
2020-05-11 21:32:26 +0900

    F value evaluation to if and while statements

---
+++ fitness.py
@@ -0,0 +1,17 @@
+import ast
+
+def fitness_pred(pred):
+    # print(pred)
+    # TODO Need to handle if predicate is compared to another value or to another variable
+    if isinstance(pred, ast.Eq):
+        return "fitness_func = lambda a, b, K=1: abs(a - b)\nbd = fitness_func(1,2)"
+    elif isinstance(pred, ast.NotEq):
+        return "fitness_func = lambda a, b, K=1: -abs(a - b)"
+    elif isinstance(pred, ast.Lt):
+        return "fitness_func = lambda a, b, K=1: a - b + K"
+    elif isinstance(pred, ast.LtE):
+        return "fitness_func = lambda a, b, K=1: a - b + L"
+    elif isinstance(pred, ast.Gt):
+        return "fitness_func = lambda a, b, K=1: b - a + K"
+    elif isinstance(pred, ast.GtE):
+        return "fitness_func = lambda a, b, K=1: b - a + K"
--- visitor.py
+++ visitor.py
@@ -5,50 +5,90 @@ import fitness as ft
 tree = astor.parse_file("cs453-SBST-master/inputs/sample3.py")
 
 class AstLister(ast.NodeTransformer):
-    # def visit_FunctionDef(self, node: ast.FunctionDef):
-    #     # * FunctionDef(identifier name, arguments args,
-    #     # *        stmt* body, expr* decorator_list, expr? returns,
-    #     # *        string? type_comment)
-    #     # print(node.name)
-    #     self.generic_visit(node)
+    def visit_FunctionDef(self, node: ast.FunctionDef):
+        # * FunctionDef(identifier name, arguments args,
+        # *        stmt* body, expr* decorator_list, expr? returns,
+        # *        string? type_comment)
+        # * New variable to store branch distance
 
-    # def visit_If(self, node: ast.If):
-    #     # * If(expr test, stmt* body, stmt* orelse)
-    #     # print(node.test)
-    #     self.generic_visit(node)
+        branch_distance = ast.Assign(targets=[ast.Name(id='bd')], value=ast.Constant(value=0), type_comment=None)
+        node.body = [branch_distance] + node.body
+        self.generic_visit(node)
+        return node
 
-    def visit_Compare(self, node: ast.Compare):
-        # * Compare(expr left, cmpop* ops, expr* comparators)
-        # print(node.left)
-        # print(node.ops)
-        # * Each Compare node has:
-        # * left -> left value
-        # * ops -> operators
-        # * comparators -> list of values after the first 
-        a = node.left
-        b = node.comparators[0]
-        for op in node.ops:
-            fit = ft.fitness_pred(op)
-            print(fit(1, 2, 1), " line_no ", node.lineno)
-        # print(node.comparators)
-        # self.generic_visit(node)
-        ret_node = ast.Compare()
-        ret_node.left = node.comparators[0]
-        ret_node.ops = node.ops
-        ret_node.comparators = [node.left]
-        # print(node.left, ret_node.left)
-        # print(node.comparators, ret_node.comparators)
-        # self.generic_visit(node)
-        # return ast.copy_location(ret_node, node)
-        # print(ast.dump(ret_node))
-        return ret_node
-
-    # def visit_While(self, node: ast.While):
+    def visit_If(self, node: ast.If):
+        # * If(expr test, stmt* body, stmt* orelse)
+        # print(node.test)
+        comp = node.test
+        fit = ft.fitness_pred(comp.ops[0])
+        func = ast.parse(fit).body[0]
+        # TODO
+        # ! AFTER GETTING THE FUNCTION, WE NEED TO ASSIGN IT TO BRANCH_DISTANCE
+        # ? Maybe turn this code into a function to reduce boilerplate?
+        left = comp.left
+        right = comp.comparators[0]
+        branch_distance = ast.Assign(targets=[ast.Name(id='bd')], type_comment=None)
+        branch_distance.value = ast.Call(func=ast.Name(id='fitness_func'), args=[left, right], keywords=[])
+        # branch_distance.value = ast.Call(func=ast.Name(id='fitness_func'), args=[ast.Constant(value=1, kind=None), ast.Constant(value=1, kind=None), right], keywords=[])
+        # branch_distance.args = [left, right] # * Here we need to actually pass the correct values/arguments
+        # print(astor.dump_tree(branch_distance))
+        # print(astor.to_source(branch_distance))
+        self.generic_visit(node)
+        return func, branch_distance, node
+        # return func, node
         
-    #     # print(node.test)
-    #     # print(node.body)
-    #     # print(node.orelse)
-    #     self.generic_visit(node)
+
+    # def visit_Compare(self, node: ast.Compare):
+    #     # * Compare(expr left, cmpop* ops, expr* comparators)
+    #     # print(node.left)
+    #     # print(node.ops)
+    #     # * Each Compare node has:
+    #     # * left -> left value
+    #     # * ops -> operators
+    #     # * comparators -> list of values after the first 
+    #     for op in node.ops:
+    #         fit = ft.fitness_pred(op)
+    #         func = ast.parse(fit).body[0]
+    #         # print(self)
+    #         # print(astor.dump_tree(ast.parse(fit)))
+    #         # print('---')
+    #         # print(fit)
+    #         # print(astor.dump_tree(ast.parse(fit)), "DUMPED")
+    #         # print(fit(1, 2, 1), " line_no ", node.lineno)
+    #     # print(node.comparators)
+    #     # self.generic_visit(node)
+    #     # ret_node = ast.Compare()
+    #     # ret_node.left = node.comparators[0]
+    #     # ret_node.ops = node.ops
+    #     # ret_node.comparators = [node.left]
+    #     # print(node.left, ret_node.left)
+    #     # print(node.comparators, ret_node.comparators)
+    #     # self.generic_visit(node)
+    #     # return ast.copy_location(ret_node, node)
+    #     # print(ast.dump(ret_node))
+    #     return node
+
+    def visit_While(self, node: ast.While):
+        # * TypeDef: While(expr test, stmt* body, stmt* orelse)
+        # print(node.test)
+        # print(node.body)
+        # print(node.orelse)
+        # self.generic_visit(node)
+        comp = node.test
+        fit = ft.fitness_pred(comp.ops[0])
+        func = ast.parse(fit).body[0]
+        left = comp.left
+        right = comp.comparators[0]
+        branch_distance = ast.Assign(targets=[ast.Name(id='bd')], type_comment=None)
+        branch_distance.value = ast.Call(func=ast.Name(id='fitness_func'), args=[left, right], keywords=[])
+        self.generic_visit(node)
+        return func, branch_distance, node
+
+    def visit_Return(self, node: ast.Return):
+        # * TypeDef: Return(expr? value)
+        orig_return = node.value
+        to_return = ast.Return(value=ast.Tuple(elts=[orig_return, ast.Name(id='bd')]))\
+        return to_return
 
     # def visit_For(self, node: ast.For):
     #     # print(node.target)
@@ -85,3 +125,6 @@ ast.fix_missing_locations(newtree)
 print(astor.to_source(newtree))
 # code = compile(newtree, "<string>", "exec")
 # exec(code)
+# print(astor.dump_tree(ast.parse('bd = fitness_func(1,2)')))
+print(astor.dump_tree(ast.parse('return x, bd')))
+# print(astor.dump_tree(tree))
