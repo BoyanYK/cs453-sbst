059aa664fd2303c0c73edbc84fda39eebdf4d2d9
BoyanYK <28277932+BoyanYK@users.noreply.github.com>
2020-05-21 17:51:48 +0900

    Branch execution tracing

--- utils.py
+++ utils.py
@@ -19,3 +19,32 @@ def comp_to_bd(comp: ast.Compare):
     branch_distance = ast.Assign(targets=[ast.Name(id='bd', ctx=ast.Store())], type_comment=None)
     branch_distance.value = ast.Call(func=ast.Name(id='fitness_func', ctx=ast.Load()), args=[left, right], keywords=[])
     return func, branch_distance
+
+def enumerate_branch(node, branch):
+    """ 
+    Given an AST node, checks if the node has branching and if not, marks its body and/or else 
+    statements as branches
+    Arguments:
+        node {ast element} -- AST node to verify
+        branch {int} -- Current branch number
+
+    Returns:
+        ({ast element}, {int}) -- returns the same structure as input in order to continue iteration
+    """
+    has_branch = False
+    for stmt in node.body:
+        if isinstance(stmt, ast.If) or isinstance(stmt, ast.While):
+            has_branch = True
+            break
+
+    if not has_branch:
+        branch_counter = ast.parse("branch_ids_visited.add({})".format(branch)).body[0]
+        branch += 1
+        node.body = [branch_counter] + node.body
+        if node.orelse:
+            branch_counter = ast.parse("branch_ids_visited.add({})".format(branch)).body[0]
+            # print(ast.dump(branch_counter))
+            branch += 1
+            node.orelse = [branch_counter] + node.orelse
+        
+    return node, branch
--- visitor.py
+++ visitor.py
@@ -1,14 +1,22 @@
 import ast
 import astor
 
-from utils import comp_to_bd
+from utils import comp_to_bd, enumerate_branch
 
 tree = astor.parse_file("cs453-SBST-master/inputs/sample3.py")
 
 class AstInstrumenation(ast.NodeTransformer):
     def __init__(self):
-        self.body_len = 0
+        # self.body_len = 0
         self.branch = 0
+
+    def get_max_branch(self):
+        """Helper to get the maximum branch number for a function
+
+        Returns:
+            {int} -- Maximum branch
+        """
+        return self.branch 
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
         # * TypeDef: FunctionDef(identifier name, arguments args,
@@ -16,7 +24,8 @@ class AstInstrumenation(ast.NodeTransformer):
         # *             string? type_comment)
         # Initialize variable to store the branch distance in
         branch_distance = ast.Assign(targets=[ast.Name(id='bd', ctx=ast.Store())], value=ast.Constant(value=0), type_comment=None)
-        node.body = [branch_distance] + node.body
+        branch_id = ast.parse("branch_ids_visited = set()").body[0]
+        node.body = [branch_distance, branch_id] + node.body
         self.generic_visit(node)
         return node
 
@@ -24,18 +33,7 @@ class AstInstrumenation(ast.NodeTransformer):
         # * TypeDef: If(expr test, stmt* body, stmt* orelse)
         func, branch_distance = comp_to_bd(node.test)
 
-        has_branch = False
-        for stmt in node.body:
-            if isinstance(stmt, ast.If) or isinstance(stmt, ast.While):
-                has_branch = True
-                break
-
-        if not has_branch:
-            branch_counter = ast.parse("branch_id = {}".format(self.branch))
-            self.branch += 1
-            node.body = [branch_counter] + node.body
-            if node.orelse:
-                node.orelse = [branch_counter] + node.orelse
+        node, self.branch = enumerate_branch(node, self.branch)
 
         self.body_len = len(node.body)
 
@@ -46,29 +44,24 @@ class AstInstrumenation(ast.NodeTransformer):
         # * TypeDef: While(expr test, stmt* body, stmt* orelse)
         func, branch_distance = comp_to_bd(node.test)
 
-        has_branch = False
-        for stmt in node.body:
-            if isinstance(stmt, ast.If) or isinstance(stmt, ast.While):
-                has_branch = True
-                break
-
-        if not has_branch:
-            branch_counter = ast.parse("branch_id = {}".format(self.branch))
-            self.branch += 1
-            node.body = [branch_counter] + node.body
-            if node.orelse:
-                node.orelse = [branch_counter] + node.orelse
+        node, self.branch = enumerate_branch(node, self.branch)
 
         self.generic_visit(node)
         return func, branch_distance, node
 
+    def visit_For(self, node: ast.For):
+        node, self.branch = enumerate_branch(node, self.branch)
+        
+        self.generic_visit(node)
+        return node
+
     def visit_Return(self, node: ast.Return):
         # * TypeDef: Return(expr? value)
         orig_return = node.value
-        print(self.body_len)
-        self.body_len = 0
+        # print(self.body_len)
+        # self.body_len = 0
         # Append branch distance to return operator
-        to_return = ast.Return(value=ast.Tuple(elts=[orig_return, ast.Name(id='bd', ctx=ast.Load())], ctx=ast.Load()))
+        to_return = ast.Return(value=ast.Tuple(elts=[orig_return, ast.Name(id='bd', ctx=ast.Load()), ast.Name(id='branch_ids_visited', ctx=ast.Load())], ctx=ast.Load()))
         return to_return
 
 # TODO When recursing over the tree, need to store the number of arguments the function takes, as well as the function name 
@@ -77,9 +70,9 @@ ast.fix_missing_locations(newtree)
 # print(astor.dump_tree(newtree))
 
 # TODO MAYBE ITERATE ONCE MORE IN BFS FASHION SO THAT YOU CAN GET DEPTH OF ELEMENTS?
-# code = compile(newtree, filename='<blah>', mode='exec')
-# namespace = {}
-# exec(code, namespace)
-# print(namespace['test_me'](4))
+code = compile(newtree, filename='<blah>', mode='exec')
+namespace = {}
+exec(code, namespace)
+print(namespace['test_me'](4))
 
-print(astor.to_source(newtree))
+# print(astor.to_source(newtree))
