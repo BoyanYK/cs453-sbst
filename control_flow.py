dc72d35fc0bf70b9eefa00e99de22cce72147678
BoyanYK <28277932+BoyanYK@users.noreply.github.com>
2020-05-30 17:47:36 +0900

    Hill-climbing for single argument function

---
+++ control_flow.py
@@ -0,0 +1,73 @@
+import ast
+
+class Node(object):
+    def __init__(self, node, parent=None):
+        self.name = node.__class__.__name__
+        self.parent = parent
+        self.depth = 0 if not self.parent else self.parent.depth +  1
+        self.lineno = node.lineno
+        self.node = node
+        
+        self.children = []
+
+    def __str__(self):
+        # return "Node {}, depth {}, line {}".format(self.name, self.depth, self.lineno)
+        return "{} @ {}".format(self.name, self.lineno)
+
+    def get_body(self):
+        try:
+            return self.node.body
+        except Exception:
+            return []
+
+    def get_else(self):
+        try:
+            return self.node.orelse
+        except Exception:
+            return []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def compare(self, target):
+        return self.name == target[0] and self.lineno == target[3]
+
+def get_custom_tree(tree):
+    function = Node(tree.body[0], 0)
+    queue = []
+    queue.append(function)
+    flow_change = []
+    while queue:
+        node = queue.pop(0)
+        for child in node.get_body():
+            child_node = Node(child, parent=node)
+            queue.append(child_node)
+            node.add_child(child_node)
+
+        if isinstance(node.node, ast.If) or isinstance(node.node, ast.While) or isinstance(node.node, ast.For):
+            for child in node.get_else():
+                child_node = Node(child, parent=node)
+                queue.append(child_node)
+                node.add_child(child_node)
+
+        if isinstance(node.node, ast.If) or isinstance(node.node, ast.While):# Can't deal with For for now or isinstance(node.node, ast.For):
+            flow_change.append(node)
+    return function, flow_change
+
+def get_targets(tree):
+    root, flow_change = get_custom_tree(tree)
+    # print(flow_change)
+    targets = {}
+    for node in flow_change:
+        node_tree = [node]
+        iter_node = node
+        while not isinstance(iter_node.node, ast.FunctionDef):
+            parent = iter_node.parent
+            node_tree.append(parent)
+            iter_node = parent
+        targets[node] = node_tree
+    return targets
+    
--- covgen.py
+++ covgen.py
@@ -2,8 +2,9 @@ import ast
 import astor
 import visitor
 import control_flow
-import sys
+import sys, os
 import math
+import random
 
 def main():
     target_path = sys.argv[1]
@@ -13,44 +14,95 @@ def main():
 
     targets = control_flow.get_targets(target_tree)
 
-    search(instrumented, targets)
+    hill_climb(instrumented, targets)
 
-def search(tree, targets):
+# * Code from https://stackoverflow.com/questions/8391411/suppress-calls-to-print-python
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+def hill_climb(tree, targets):
+    import numpy as np
     # TODO Loop for all targets ..
-    # for target, path in targets.items():
-    path = list(targets.values())[2]
-    print(path)
-    path = list(reversed(path))[1:] # Removes function_def node
+    for target, path in targets.items():
+    # path = list(targets.values())[0]
+    # print(path)
+        path = list(reversed(path))[1:] # Removes function_def node
+    # trace = try_input(tree, 1)
+    # fitness, predicate_value = calculate_fitness(trace, path)
+    
     # TODO Loop until fitness function ... 
-    trace = try_input(tree, [])
-    fitness, predicate_value = calculate_fitness(trace, path)
-    print(fitness, predicate_value)
+        curr_fitness = 10000
+        value = random.randint(0, 100)
+        # value = 3
+        visited_states = {}
+        local_minima_iter = 1
+        while True:
+            trace = try_input(tree, value)
+            new_fitness, predicate_value = calculate_fitness(trace, path)
+            visited_states[value] = new_fitness
+            if new_fitness == 0 or predicate_value:
+                break
+            else:
+                neighbours = [value - local_minima_iter, value + local_minima_iter]
+                results = {}
+                results[value] = new_fitness
+                for neighbour in neighbours:
+                    if neighbour in visited_states:
+                        results[neighbour] = visited_states[neighbour]
+                    else:
+                        trace = try_input(tree, neighbour)
+                        neighb_fitness, neighb_predicate_value = calculate_fitness(trace, path)
+                        results[neighbour] = neighb_fitness
+                        visited_states[neighbour] = neighb_fitness
+                # print(value)
+                index = np.argmin(list(results.values()))
+                old_value = value
+                value = list(results.keys())[index]
+                # if value == new_fitness:
+                #     local_minima_iter += 1
+                # else:
+                #     local_minima_iter = 1
+                local_minima_iter = 1 if value != old_value else local_minima_iter + 1
+                # print(value)
+            # print("Current value {}, fitness {}".format(value, results[value]))
+            # break
+
+        print(target, value, new_fitness, predicate_value)
     # TODO New inputs based on fitness.. 
 
-def try_input(tree, inputs):
+def try_input(tree, input):
     trace = []
     code = compile(tree, filename='<blah>', mode='exec')
     namespace = {}
     exec(code, namespace)
     # TODO Get name of function automatically
     # TODO Pass correct number of arguments
-    namespace['test_me'](0, trace)
+    blockPrint()
+    namespace['test_me'](input, trace)
+    enablePrint()
     return trace
 
 def calculate_fitness(trace, path):
-    approach = None, len(path) - 1
+    approach = ('None', 10000, None, None), len(path) - 1
+    # print(trace)
+    # print(path)
     for entry in trace:
         for stop in path:
             if stop.compare(entry):
                 approach_level = len(path) - 1 - path.index(stop)
-                if approach_level < approach[1]:
+                if approach_level < approach[1] or approach_level == 0:
                     approach = entry, approach_level
                 break
     approach_level = approach[1]
     branch_distance = approach[0][1]
+    # print(branch_distance)
     fitness = approach_level + (1 - math.pow(1.001, -branch_distance)) 
-    # print("Approach ", approach)
-    return fitness, approach[0][2]
+    return fitness, approach[0][2] 
 
 
 if __name__ == "__main__":
